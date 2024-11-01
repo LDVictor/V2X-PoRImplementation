94eeba8678a0a04a44d8135555adc485a29b6ac8
Victor Emanuel <victor.borges@ccc.ufcg.edu.br>
2024-11-01 15:41:12 -0300

    Correção de bug no cálculo do índice de confiança.

--- values.py
+++ values.py
@@ -5,6 +5,12 @@ t_v = timestamp = t_approx = t_current = rep = 0
 d_approx = d_ran = []
 n = random.randint(1, 20)
 
+def clean_lists():
+    fpor_s.clear()
+    c_s.clear()
+    d_approx.clear()
+    d_ran.clear()
+
 def generate_fpor_s():
     for i in range(0, n):
         value = random.random()
@@ -24,12 +30,12 @@ def generate_times():
 
 def generate_coordinates():
     # coordenadas variam de 0 ate 180 graus
-    d_approx0 = random.random() * random.randint(0, 180)
-    d_approx1 = random.random() * random.randint(0, 180)
+    d_approx0 = random.random() * random.randint(1, 180)
+    d_approx1 = random.random() * random.randint(1, 180)
     d_approx.append(d_approx0)
     d_approx.append(d_approx1)
-    d_ran0 = random.random() * random.randint(0, 180)
-    d_ran1 = random.random() * random.randint(0, 180)
+    d_ran0 = random.random() * random.randint(1, 180)
+    d_ran1 = random.random() * random.randint(1, 180)
     d_ran.append(d_ran0)
     d_ran.append(d_ran1)
 
@@ -38,6 +44,7 @@ def generate_rep():
     rep = random.random() * random.randint(0, 5)
 
 def fill_values():
+    clean_lists()
     generate_fpor_s()
     generate_c_s()
     generate_times()
